d77857e247e6766230eb6f9ca23f5ec791b8a8af
jmbenso2 <gogglebrains@gmail.com>
2026-05-17 19:17:37 -0400

    Fixed cycleable weapons count

--- src/include/constants.h
+++ src/include/constants.h
@@ -30,7 +30,7 @@
 #define MAX_EXPLOSIONS_DEFAULT 6
 
 #define NUM_BOTDIFFS          6
-#define NUM_CYCLEABLE_WEAPONS 45
+#define NUM_CYCLEABLE_WEAPONS 47
 #define NUM_DEVICETESTS       10
 #define NUM_FRAMEBUFFERS      2
 #define NUM_GFXTASKS          2
